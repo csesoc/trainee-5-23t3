7a49ff60919a553453cd41e86243a5d1bc1fb8f5
NailTheHogRider <alex.lee@naillab.com.au>
2023-10-26 11:19:54 +1100

    backend setup completed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,59 @@
-hi
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import { InputError, AccessError } from './error';
+import {
+  echoFunction
+} from './service'
+
+const PORT = 6969;
+
+const app = express();
+app.use(cors({
+  origin: (origin, callback) => {
+    callback(null, true);
+  },
+  credentials: true
+}));
+app.use(express.json());
+
+const errorHandler = (fn: (req: Request, res: Response, next: NextFunction) => Promise<void>) => 
+  async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await fn(req, res, next);
+  } catch (err) {
+    if (err instanceof InputError) {
+      res.status(400).send({ error: err.message });
+    } else if (err instanceof AccessError) {
+      res.status(403).send({ error: err.message });
+    } else {
+      console.log(err);
+      res.status(500).send({ error: 'Internal Server Error' });
+    }
+  }
+};
+
+// Route Handlers Here
+app.get('/', (req, res) => {
+    res.json('Hello World');
+});
+
+app.post(
+  '/echo',
+  errorHandler(async (req, res) => {
+    const { startup } = req.body;
+    const response = await echoFunction(startup);
+    res.json(response);
+  }),
+);
+
+
+// Default route for unmatched routes
+app.use((req, res) => {
+    res.status(404).send({ error: 'Not Found' });
+});
+
+// Route Handlers End here
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
