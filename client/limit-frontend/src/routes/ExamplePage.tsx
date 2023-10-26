cad40e944516dcd9d53a38682abec55fdfcbd379
Dylan Huynh <redman29h@gmail.com>
2023-10-26 13:56:07 +1100

    Updated run dev to work off nodemon, allow's refresh

--- client/limit-frontend/src/routes/ExamplePage.tsx
+++ client/limit-frontend/src/routes/ExamplePage.tsx
@@ -1,11 +1,26 @@
 import { Autocomplete, ColorSwatch, Group } from "@mantine/core";
+import { useEffect, useState } from "react";
 
 export default function ExamplePage() {
+    const [ text, setText ] = useState('')
+    useEffect(() => {
+          fetch(`http://localhost:6969/`)
+          .then(resp => resp.json())
+          .then(data => {
+           setText(data)
+          }).catch(error => {
+            console.log(error)
+          })
+      }, [])
+
     return (
         <>
             <h2>
                 This is a funny example Page
             </h2>
+            <h3>
+                Your Funny Text is {text}
+            </h3>
             <Autocomplete
                 label="Your favorite library"
                 placeholder="Pick value or enter anything"
